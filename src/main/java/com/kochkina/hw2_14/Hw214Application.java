947d0bb9386b0a722e4243e2a10bbc817b797548
Anastasiya Kochkina <kochkinaanastasiya@yandex.ru>
2022-12-08 11:24:16 +0300

    first commit

---
+++ src/main/java/com/kochkina/hw2_14/Hw214Application.java
@@ -0,0 +1,13 @@
+package com.kochkina.hw2_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw214Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw214Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kochkina/hw2_14/Hw214ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kochkina.hw2_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw214ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
